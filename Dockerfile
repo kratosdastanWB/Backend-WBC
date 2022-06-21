414fc4d65e95295171eea2e622e213b6f33d7a5e
kratosdastan97 <cristofer_gf@hotmail.com>
2022-06-21 10:30:40 -0500

    Conexion de base de datos y algunos avances de models y migrate

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.8-slim
 WORKDIR /app
 ENV FLASK_APP app.py
 COPY requeriments.txt requeriments.txt
+RUN /usr/local/bin/python -m pip install --upgrade pip
 RUN pip install -r requeriments.txt
 COPY . /app
 EXPOSE 5000
--- app.py
+++ app.py
@@ -1,11 +1,44 @@
+from cmath import log
+import string
 import time
-
+from venv import create
 import redis
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request
+from sqlalchemy import create_engine
+import os
+import logging
 
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+logging.debug("Se incio esto")
+
 cache = redis.Redis(host='redis', port=6379)
+
+def execute_queries(query):
+    logging.debug("se inicia la DB")
+    db_name = 'WBC'
+    db_user = 'postgres'
+    db_pass = 'example'
+    db_host = 'db'
+    db_port = '5432'
+    db_string = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
+    db = create_engine(db_string)
+    try:
+        data = db.execute(query)
+        for item in data:
+            logging.debug(item)
+    except Exception as ex:
+        logging.debug(ex)  
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method=='POST':
+        return jsonify({"message": "Resultado exitoso",
+        "username": request.form['username'],
+        "password": request.form['password']})
+    return ''    
 
 def get_hit_count():
     retries = 5
@@ -20,5 +53,6 @@ def get_hit_count():
 
 @app.route('/')
 def hello():
+    execute_queries("CREATE TABLE users ( ID SERIAL PRIMARY KEY, NAME varchar(50));")
     count = get_hit_count()
     return 'Hello World! haz visitado {} veces este sitio.\n'.format(count)
